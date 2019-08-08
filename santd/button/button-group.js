bdabd3ba900cd57d3d55160f1357358635e8ebf0
errorrik <errorrik@gmail.com>
2019-08-08 16:22:42 +0800

    opti classes computed, and PREFIX_CLASS gen

--- santd/button/button-group.js
+++ santd/button/button-group.js
@@ -1,14 +1,15 @@
 /**
-* @file button group
-* @author fuqiangqiang@baidu.com
-*/
+ * @file button group
+ * @author fuqiangqiang@baidu.com
+ */
 import san, {DataTypes} from 'san';
-const prefixCls = 'san-btn-group';
+const PREFIX_CLASS = classCreator('btn-group')();
 
 export default san.defineComponent({
     dataTypes: {
         size: DataTypes.string
     },
+
     initData() {
         return {
             sizeMap: {
@@ -17,27 +18,19 @@ export default san.defineComponent({
             }
         };
     },
+
     computed: {
         classes() {
-            const data = this.data;
-            const size = data.get('sizeMap')[data.get('size')];
-            const res = {
-                [`${prefixCls}`]: true,
-                [`${prefixCls}-${size}`]: !!size
-            };
-            let arr = [];
-            for (let item in res) {
-                if (!!res[item]) {
-                    arr.push(item);
-                }
-            }
-            arr.push(this.data.get('className'));
+            let arr = [PREFIX_CLASS, this.data.get('className')];
+
+            let size = this.data.get('sizeMap')[this.data.get('size')];
+            size && arr.push(`${PREFIX_CLASS}-${size}`);
+
             return arr;
         }
     },
+
     template: `
-        <div class="{{classes}}" style="font-size:0;">
-            <slot></slot>
-        </div>
+        <div class="{{classes}}" style="font-size:0;"><slot/></div>
     `
 });
