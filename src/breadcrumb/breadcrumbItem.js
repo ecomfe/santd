2349928385d7c3f9ea8d439596fd53c716a2f160
errorrik <errorrik@gmail.com>
2019-08-29 17:46:05 +0800

    opti if expr

--- src/breadcrumb/breadcrumbItem.js
+++ src/breadcrumb/breadcrumbItem.js
@@ -6,21 +6,25 @@
 import san, {DataTypes} from 'san';
 import {classCreator} from '../core/util';
 const prefixCls = classCreator('breadcrumb')();
+
 export default san.defineComponent({
     dataTypes: {
         href: DataTypes.string
     },
+
     initData() {
         return {
             separator: '/'
         };
     },
+
     created() {
         this.dispatch('santd_breadcrumb_add', this);
     },
+    
     template: `
         <span>
-            <a s-if="{{href}}" href="{{href}}" class="${prefixCls}-link"><slot/></a>
+            <a s-if="href" href="{{href}}" class="${prefixCls}-link"><slot/></a>
             <span s-else class="${prefixCls}-link"><slot/></span>
             <span class="${prefixCls}-separator">{{separator}}</span>
         </span>
