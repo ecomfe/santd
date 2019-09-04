b21f2405f925ea23357dcf303f1ac7a79d8164a8
errorrik <errorrik@gmail.com>
2019-09-04 11:14:32 +0800

    redo opit, which is covered on merge

--- src/avatar/index.js
+++ src/avatar/index.js
@@ -71,7 +71,7 @@ export default san.defineComponent({
         if (childrenNode) {
             const childrenWidth = childrenNode.offsetWidth;
             const avatarWidth = this.el.getBoundingClientRect().width;
-            const scale = (avatarWidth - 8 < childrenWidth) ? (avatarWidth - 8) / childrenWidth : 1;
+            const scale = (avatarWidth - 8 < childrenWidth) || 1;
 
             const transformString = `scale(${scale}) translateX(-50%)`;
             let scaleStyle = [
@@ -88,7 +88,7 @@ export default san.defineComponent({
         }
     },
 
-    attached() {
+    inited() {
         if (this.sourceSlots.noname && this.sourceSlots.noname.length) {
             this.data.set('hasSlot', true);
         }
