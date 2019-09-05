f401b9f2c955dfc132d369c95555bb0318b37626
errorrik <errorrik@gmail.com>
2019-09-05 20:01:32 +0800

    if directive should not be wrapped by {{...}}

--- src/card/meta.js
+++ src/card/meta.js
@@ -11,14 +11,14 @@ const prefixCls = classCreator('card')();
 export default san.defineComponent({
     template: `
     	<div class="${prefixCls}-meta">
-            <div s-if="{{isAvatar}}" class="${prefixCls}-meta-avatar">
+            <div s-if="isAvatar" class="${prefixCls}-meta-avatar">
                 <slot name="avatar" />
             </div>
-            <div s-if="{{isTitle || isDes || title || description}}" class="${prefixCls}-meta-detail">
-                <div s-if="{{isTitle || title}}" class="${prefixCls}-meta-title">
+            <div s-if="isTitle || isDes || title || description" class="${prefixCls}-meta-detail">
+                <div s-if="isTitle || title" class="${prefixCls}-meta-title">
                     <slot name="title" />{{title}}
                 </div>
-                <div s-if="{{isDes || description}}" class="${prefixCls}-meta-description">
+                <div s-if="isDes || description}}" class="${prefixCls}-meta-description">
                     <slot name="description" />{{description}}
                 </div>
             </div>
