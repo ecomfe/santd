34b94410391c147810ff669d21a6539ae7684cce
errorrik <errorrik@gmail.com>
2019-09-10 13:59:58 +0800

    remove prefixCls and className

--- src/tag/index.js
+++ src/tag/index.js
@@ -31,29 +31,29 @@ const presetColorRegex = new RegExp(`^(${presetColorTypes.join('|')})(-inverse)?
 
 const Tag = san.defineComponent({
     dataTypes: {
-        prefixCls: DataTypes.string,
-        className: DataTypes.string,
         color: DataTypes.string,
         closable: DataTypes.bool,
         visible: DataTypes.bool,
         style: DataTypes.oneOfType([DataTypes.string, DataTypes.object])
     },
+
     computed: {
         classes() {
             const visible = this.data.get('visible');
             const color = this.data.get('color');
-            const className = this.data.get('className');
             const isPresetColor = this.data.get('isPresetColor');
-            let classArr = [prefixCls, className];
+            let classArr = [prefixCls];
             isPresetColor && classArr.push(`${prefixCls}-${color}`);
             color && !isPresetColor && classArr.push(`${prefixCls}-has-color`);
             !visible && classArr.push(`${prefixCls}-hidden`);
             return classArr;
         },
+
         isPresetColor() {
             const color = this.data.get('color');
             return color ? presetColorRegex.test(color) : false;
         },
+
         tagStyle() {
             const color = this.data.get('color');
             const bodyStyle = this.data.get('bodyStyle');
@@ -64,6 +64,7 @@ const Tag = san.defineComponent({
                 ...toStyle.object(bodyStyle)
             };
         },
+
         isNeedWave() {
             const instance = this.data.get('instance');
             const children = instance && instance.sourceSlots.noname[0];
@@ -71,17 +72,20 @@ const Tag = san.defineComponent({
             return children && children.tagName === 'a';
         }
     },
+
     initData() {
         return {
             closable: false,
             visible: true
         };
     },
+
     inited() {
         this.data.set('bodyStyle', this.data.get('style'));
         this.data.set('style', {});
         this.data.set('instance', this);
     },
+
     setVisible(visible, e) {
         this.fire('close', e);
         if (e.defaultPrevented) {
@@ -89,21 +93,25 @@ const Tag = san.defineComponent({
         }
         this.data.set('visible', visible);
     },
+
     handleIconClick(e) {
         this.setVisible(false, e);
     },
+
     handleClick(e) {
         this.fire('click', e);
     },
+
     components: {
         's-wave': Wave,
         's-icon': Icon
     },
+
     template: `
         <div class="{{classes}}" style="{{tagStyle}}" on-click="handleClick">
-            <slot></slot>
+            <slot />
             <s-icon type="close" on-click="handleIconClick" s-if="closable" />
-            <s-wave s-if="isNeedWave"></s-wave>
+            <s-wave s-if="isNeedWave" />
         </div>
     `
 });
