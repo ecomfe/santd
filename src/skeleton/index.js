15bb932f84e27c3b346751f8a746db1ba1af4f3b
Fisherma <frostm@163.com>
2019-09-19 18:52:08 +0800

    remove useless code

--- src/skeleton/index.js
+++ src/skeleton/index.js
@@ -8,71 +8,70 @@ import './style/index.less';
 import san, {DataTypes} from 'san';
 import {classCreator} from '../core/util';
 
-import Avatar, {SkeletonAvatarProps} from './Avatar';
-import Title, {SkeletonTitleProps} from './Title';
-import Paragraph, {SkeletonParagraphProps} from './Paragraph';
-
 const prefixCls = classCreator('skeleton')();
-
-function getComponentProps(prop) {
-    return prop && typeof prop === 'object' ? prop : {};
-}
-
-function getAvatarBasicProps(hasTitle, hasParagraph) {
-    return {
-        shape: hasTitle && !hasParagraph ? 'square' : 'circle',
-        size: 'large'
-    };
-}
-
-function getTitleBasicProps(hasAvatar, hasParagraph) {
-    if (hasParagraph) {
-        return {
-            width: !hasAvatar ? '38%' : '50%'
-        };
-    }
-    return {};
-}
-
-function getParagraphBasicProps(hasAvatar, hasTitle) {
-    let basicProps = {};
-
-    if (!hasAvatar || !hasTitle) {
-        basicProps.width = '61%';
-    }
-    basicProps.rows = !hasAvatar && hasTitle ? 3 : 2;
-    return basicProps;
-}
+const widthUnit = DataTypes.oneOfType([DataTypes.number, DataTypes.string]);
 
 export default san.defineComponent({
     template: `
         <div style="display: table; width: 100%;">
-            <slot s-if="{{!(loading || undefined === loading)}}"/>
-            <div s-if="{{loading || undefined === loading}}" class="{{className}}">
-                <div s-if="{{avatar}}" class="${prefixCls}-header">
-                    <avatar props="{{avatarProps}}"/>
+            <slot s-if="!(loading || undefined === loading)" />
+            <div s-if="loading || undefined === loading" class="{{classes}}">
+                <div s-if="avatar" class="${prefixCls}-header">
+                    <span class="{{getAvatarClasses(avatar.shape || 'circle', avatar.size || 'large')}}" />
                 </div>
-                <div s-if="{{title || paragraph}}" class="${prefixCls}-content">
-                    <title s-if="{{title}}" props="{{titleProps}}"/>
-                    <paragraph s-if="{{paragraph}}" props="{{paragraphProps}}"/>
+                <div s-if="title || paragraph" class="${prefixCls}-content">
+                    <h3 class="${prefixCls}-title" style="width: {{title.width || !avatar ? '38%' : '50%'}}" s-if="title" />
+                    <ul class="${prefixCls}-paragraph">
+                        <li s-for="row, index in rowList" style="{{getParagraphStyle(index)}}" />
+                    </ul>
                 </div>
             </div>
         </div>
     `,
     dataTypes: {
         active: DataTypes.bool,
-        avatar: SkeletonAvatarProps,
+        avatar: DataTypes.oneOfType([
+            DataTypes.bool,
+            DataTypes.shape({
+                size: DataTypes.oneOf(['large', 'small', 'default']),
+                shape: DataTypes.oneOf(['circle', 'square'])
+            })
+        ]),
         loading: DataTypes.bool,
-        paragraph: SkeletonParagraphProps,
-        title: SkeletonTitleProps
+        paragraph: DataTypes.oneOfType([
+            DataTypes.bool,
+            DataTypes.shape({
+                rows: DataTypes.number,
+                width: DataTypes.oneOfType([widthUnit, DataTypes.arrayOf(widthUnit)])
+            })
+        ]),
+        title: DataTypes.oneOfType([
+            DataTypes.bool,
+            DataTypes.shape({width: widthUnit})
+        ])
     },
-    components: {
-        avatar: Avatar,
-        paragraph: Paragraph,
-        title: Title
+    getAvatarClasses(shape, size) {
+        const avatarPrefixCls = `${prefixCls}-avatar`;
+        let classArr = [avatarPrefixCls];
+
+        shape && classArr.push(`${avatarPrefixCls}-${shape}`);
+        size && classArr.push(`${avatarPrefixCls}-${size}`);
+
+        return classArr;
+    },
+    getParagraphStyle(index) {
+        const {width, rows = 2} = this.data.get('paragraph');
+        if (Array.isArray(width)) {
+            return `width: ${width[index]};`;
+        }
+        // last paragraph
+        if (rows - 1 === index) {
+            return `width: ${width};`;
+        }
+        return undefined;
     },
     computed: {
-        className() {
+        classes() {
             const loading = this.data.get('loading');
 
             if (loading || undefined === loading) {
@@ -84,38 +83,13 @@ export default san.defineComponent({
                 return classArr;
             }
         },
-        avatarProps() {
-            const data = this.data;
-            const avatar = data.get('avatar');
-            const title = data.get('title');
-            const paragraph = data.get('paragraph');
+        rowList() {
+            const paragraph = this.data.get('paragraph') || {};
+            const avatar = this.data.get('avatar');
+            const title = this.data.get('title');
 
-            return {
-                ...getAvatarBasicProps(!!title, !!paragraph),
-                ...getComponentProps(avatar)
-            };
-        },
-        paragraphProps() {
-            const data = this.data;
-            const paragraph = data.get('paragraph');
-            const avatar = data.get('avatar');
-            const title = data.get('title');
-
-            return {
-                ...getParagraphBasicProps(!!avatar, !!title),
-                ...getComponentProps(paragraph)
-            };
-        },
-        titleProps() {
-            const data = this.data;
-            const title = data.get('title');
-            const avatar = data.get('avatar');
-            const paragraph = data.get('paragraph');
-
-            return {
-                ...getTitleBasicProps(!!avatar, !!paragraph),
-                ...getComponentProps(title)
-            };
+            const rows = paragraph.rows || (!avatar && title ? 3 : 2);
+            return new Array(rows);
         }
     },
     initData() {
