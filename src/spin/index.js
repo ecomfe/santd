ef72fae2cdeb2b0a1e90d70548b066ee2a1298d6
Fisherma <frostm@163.com>
2019-09-19 16:33:34 +0800

    code opti

--- src/spin/index.js
+++ src/spin/index.js
@@ -15,9 +15,6 @@ const prefixCls = classCreator('spin')();
 // 默认加载器指示符
 let defaultIndicator = null;
 
-// 加载器尺寸
-const SpinSize = DataTypes.oneOf(['small', 'default', 'large']);
-
 function shouldDelay(spinning, delay) {
     return !!spinning && !!delay && !isNaN(Number(delay));
 }
@@ -44,12 +41,6 @@ const SpinDot = san.defineComponent({
     `
 });
 
-const SpinTip = san.defineComponent({
-    template: `
-        <div class="${prefixCls}-text">{{tip}}</div>
-    `
-});
-
 const IndicatorLoader = san.createComponentLoader(() => {
     const promise = new Promise(resolve => {
         resolve(isValidComponent(defaultIndicator) ? defaultIndicator : SpinDot);
@@ -59,62 +50,47 @@ const IndicatorLoader = san.createComponentLoader(() => {
 
 const Spin = san.defineComponent({
     template: `
-        <div class="{{nestedClassName || spinClassName}}">
+        <div class="{{hasContent ? '${prefixCls}-nested-loading ' + (wrapperClassName ? wrapperClassName : '') : spinClasses}}">
             <slot s-if="!hasContent" name="indicator">
-                <indicator/>
+                <indicator />
             </slot>
             <spin-tip s-if="!hasContent" tip="{{tip}}"/>
             <div s-if="hasContent && currentSpinning">
-                <div class="{{spinClassName}}">
+                <div class="{{spinClasses}}">
                     <slot name="indicator">
-                        <indicator/>
+                        <indicator />
                     </slot>
-                    <spin-tip tip="{{tip}}"/>
+                    <div class="${prefixCls}-text">{{tip}}</div>
                 </div>
             </div>
-            <div class="{{containerClassName}}">
-                <slot name="content"/>
+            <div class="${prefixCls}-container {{currentSpinning ? '${prefixCls}-blur' : ''}}">
+                <slot name="content" />
             </div>
         </div>
     `,
     dataTypes: {
         delay: DataTypes.number,
         indicator: DataTypes.any,
-        size: SpinSize,
+        size: DataTypes.oneOf(['small', 'default', 'large']),
         spinning: DataTypes.bool,
         tip: DataTypes.string,
         wrapperClassName: DataTypes.string
     },
     components: {
-        'indicator': IndicatorLoader, // san >= 3.7
-        'spin-tip': SpinTip
+        indicator: IndicatorLoader // san >= 3.7
     },
     computed: {
-        spinClassName() {
-            const data = this.data;
-            const size = data.get('size');
-            const currentSpinning = data.get('currentSpinning');
-            const tip = data.get('tip');
+        spinClasses() {
+            const size = this.data.get('size');
+            const currentSpinning = this.data.get('currentSpinning');
+            const tip = this.data.get('tip');
             let classArr = [prefixCls];
+
             size === 'small' && classArr.push(`${prefixCls}-sm`);
             size === 'large' && classArr.push(`${prefixCls}-lg`);
             currentSpinning && classArr.push(`${prefixCls}-spinning`);
             !!tip && classArr.push(`${prefixCls}-show-text`);
-            return classArr;
-        },
-        nestedClassName() {
-            const data = this.data;
-            const hasContent = data.get('hasContent');
-            const wrapperClassName = data.get('wrapperClassName');
-            let classArr = [`${prefixCls}-nested-loading`];
-            !!wrapperClassName && classArr.push(wrapperClassName);
-            return hasContent || classArr;
-        },
-        containerClassName() {
-            const data = this.data;
-            const currentSpinning = data.get('currentSpinning');
-            let classArr = [`${prefixCls}-container`];
-            currentSpinning && classArr.push(`${prefixCls}-blur`);
+
             return classArr;
         },
         currentSpinning() {
@@ -136,6 +112,7 @@ const Spin = san.defineComponent({
         const data = this.data;
         const delay = data.get('delay');
         const currentSpinning = data.get('currentSpinning');
+        this.data.set('hasContent', !!this.sourceSlots.named.content);
 
         this.watch('spinning', spinning => {
             if (currentSpinning === spinning) {
@@ -151,12 +128,14 @@ const Spin = san.defineComponent({
                 }, 200);
                 if (this.delayTimeout) {
                     clearTimeout(this.delayTimeout);
+                    this.delayTimeout = null;
                 }
             }
             else {
                 if (shouldDelay(spinning, delay)) {
                     if (this.delayTimeout) {
                         clearTimeout(this.delayTimeout);
+                        this.delayTimeout = null;
                     }
                     this.delayTimeout = window.setTimeout(this.delayUpdateSpinning.bind(this), delay);
                 }
@@ -165,13 +144,6 @@ const Spin = san.defineComponent({
                 }
             }
         });
-    },
-    attached() {
-        const innerContent = this.slot('content')[0];
-
-        if (innerContent && innerContent.isInserted) {
-            this.data.set('hasContent', true);
-        }
     },
     delayUpdateSpinning() {
         const data = this.data;
