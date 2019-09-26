a9c96d590ea11e176729475176eacefa5f7d92b9
errorrik <errorrik@gmail.com>
2019-09-26 21:28:06 +0800

    fix literal

--- src/localeprovider/index.js
+++ src/localeprovider/index.js
@@ -17,7 +17,7 @@ export default san.defineComponent({
     },
 
     inited() {
-        this.reveviers = [];
+        this.receivers = [];
 
         const locale = this.data.get('locale');
         if (locale && locale.locale) {
@@ -29,20 +29,20 @@ export default san.defineComponent({
     },
 
     disposed() {
-        this.reveviers = null;
+        this.receivers = null;
     },
 
     updated() {
         const locale = this.data.get('locale');
 
-        this.reveviers.forEach(child => {
+        this.receivers.forEach(child => {
             child.data.set('localeContext', locale);
         });
     },
 
     messages: {
         santd_add_locale_receiver(payload) {
-            this.reveviers.push(payload.value);
+            this.receivers.push(payload.value);
         }
     },
 
