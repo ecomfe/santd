2d9980973d83bc6d6e88da4ab8248dad72aa81c5
errorrik <errorrik@gmail.com>
2019-10-10 15:12:12 +0800

    opti computed, and remove useless dataType

--- src/slider/src/common/track.js
+++ src/slider/src/common/track.js
@@ -8,32 +8,22 @@ import san, {DataTypes} from 'san';
 export default san.defineComponent({
     dataTypes: {
         prefixCls: DataTypes.string,
-        included: DataTypes.bool,
         length: DataTypes.number,
         offset: DataTypes.number,
         vertical: DataTypes.bool
     },
+
     computed: {
         bodyStyle() {
-            const style = this.data.get('style');
             const vertical = this.data.get('vertical');
             const offset = this.data.get('offset');
             const length = this.data.get('length');
 
-            const positionStyle = vertical ? {
-                bottom: `${offset}%`,
-                height: `${length}%`
-            } : {
-                left: `${offset}%`,
-                width: `${length}%`
-            };
-
-            return {
-                ...style,
-                ...positionStyle
-            };
+            return vertical 
+                ? `bottom: ${offset}%; height: ${length}%`
+                : `left: ${offset}%; width: ${length}%`;
         }
     },
-    template: `<div class="{{prefixCls}}" style="{{bodyStyle}}">
-    </div>`
+
+    template: '<div class="{{prefixCls}}" style="{{bodyStyle}}"></div>'
 });
