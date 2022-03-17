554e7c54e70e9dea78028da6c65dedd665189308
Chen <chenshaoping01@baidu.com>
2022-03-17 20:57:23 +0800

    fix: 优化Form组件force选项

--- src/form/Form.js
+++ src/form/Form.js
@@ -66,7 +66,7 @@ let form = san.defineComponent({
     },
     updated() {
         this.items.forEach(item => {
-            item.data.set('form', this.form || this, {force: true});
+            item.data.set('form', this.form || this);
         });
     },
     attached() {
