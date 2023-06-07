c57465457ad4cb1d6e1b3e1385795ce584ba9dbb
xuejunru <xuejunru@baidu.com>
2023-06-07 20:12:38 +0800

    feature:add Component (Switch)

---
+++ src/switch/interface.ts
@@ -0,0 +1,17 @@
+interface SizeMap {
+    [tag: string]: string;
+}
+export interface State {
+    sizeMap: SizeMap,
+    disabled: boolean,
+    defaultChecked: boolean,
+    checked: boolean,
+    loading: boolean
+}
+
+export interface Props {
+}
+
+export interface Computed {
+
+}
