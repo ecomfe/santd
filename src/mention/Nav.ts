6b28f63f17f142153fcddb0a797c56e397dbd4db
xuejunru <xuejunru@baidu.com>
2023-06-09 16:57:09 +0800

    feature:add Component (Mention)

--- src/mention/Nav.ts
+++ src/mention/Nav.ts
@@ -2,20 +2,20 @@
  * @file 组件 Nav
  * @author zhangtingting12 <zhangtingting12@baidu.com>
  */
-import san from 'san';
 import {classCreator} from '../core/util';
 import './style/index.less';
+import Base from 'santd/base';
 
 const prefixCls = classCreator('dropdown')();
 
-export default san.defineComponent({
+export default class Nav extends Base {
     handleClick() {
         const value = this.data.get('value');
-        const dispatchData = value ? value : this.el.innerText;
+        const dispatchData = value ? value : (this.el as unknown as HTMLDivElement).innerText;
         this.dispatch('santd_mention_itemSelect', dispatchData);
-    },
+    }
 
-    template: `<div class="${prefixCls}-menu-item" on-click="handleClick">
+   static template = `<div class="${prefixCls}-menu-item" on-click="handleClick">
             <slot></slot>
         </div>`
-});
+};
--- src/mention/Suggestions.ts
+++ src/mention/Suggestions.ts
@@ -4,57 +4,17 @@
  */
 
 import './style/index.less';
-import san, {DataTypes} from 'san';
 import Icon from '../icon';
 import Nav from './Nav';
 import {classCreator} from '../core/util';
+import Base from 'santd/base';
 
 const prefixCls = classCreator('suggestions')();
 const prefixDropCls = classCreator('dropdown')();
 
-export default san.defineComponent({
-    dataTypes: {
-        placement: DataTypes.string,
-        position: DataTypes.object,
-        isShowSug: DataTypes.bool,
-        loading: DataTypes.bool
-    },
-    components: {
-        's-icon': Icon,
-        's-nav': Nav
-    },
-    computed: {
-        innerStyle() {
-            const isShowSug = this.data.get('isShowSug');
-            const position = this.data.get('position');
-            const isTop = this.data.get('placement') === 'top';
-            return {
-                display: isShowSug ? 'block' : 'none',
-                left: `${position.left}px`,
-                top: isTop ? 'auto' : `${position.top + position.height}px`,
-                bottom: isTop ? `${position.top + position.height}px;` : 'auto'
-            };
-        }
-    },
-    initData() {
-        return {
-            position: {
-                left: 0,
-                top: 0,
-                height: 0
-            },
-            display: 'none',
-            loading: false
-        };
-    },
-    created() {
-        const suggestions = this.data.get('suggestions');
-        if (typeof suggestions === 'function') {
-            this.components.customsuggestions = suggestions;
-            this.data.set('customSuggestions', true);
-        }
-    },
-    template: `
+export default class Suggestions extends Base {
+    components!: {customsuggestions: string[]}
+    static template = `
         <div class="${prefixCls}-wrapper" style="{{innerStyle}}">
             <div class="${prefixCls}-content ${prefixDropCls}-menu" s-ref="nav-area">
                 <customsuggestions s-if="{{customSuggestions}}" />
@@ -66,4 +26,39 @@ export default san.defineComponent({
             </div>
         </div>
         `
-});
+    static components = {
+        's-icon': Icon,
+        's-nav': Nav
+    }
+    static computed = {
+        innerStyle(this: Suggestions) {
+            const isShowSug = this.data.get('isShowSug');
+            const position = this.data.get('position');
+            const isTop = this.data.get('placement') === 'top';
+            return {
+                display: isShowSug ? 'block' : 'none',
+                left: `${position.left}px`,
+                top: isTop ? 'auto' : `${position.top + position.height}px`,
+                bottom: isTop ? `${position.top + position.height}px;` : 'auto'
+            };
+        }
+    }
+    initData() {
+        return {
+            position: {
+                left: 0,
+                top: 0,
+                height: 0
+            },
+            display: 'none',
+            loading: false
+        };
+    }
+    created() {
+        const suggestions = this.data.get('suggestions');
+        if (typeof suggestions === 'function') {
+            this.components.customsuggestions = suggestions;
+            this.data.set('customSuggestions', true);
+        }
+    }
+};
